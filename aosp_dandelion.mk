195554b94fc94eb0b8a4878676615334ff9daf2c
Justin Yun <justinyun@google.com>
2019-11-15 16:08:00 +0900

    dandelion: Inherit base system_ext.mk files for aosp targets
    Make aosp targets inherit handheld_system_ext.mk and
    telephony_system_ext.mk files.
    Devices that have /system_ext or /system/system_ext must inherit any
    of *_system_ext.mk files to install mandatory packages for system_ext
    partition.
    
    Bug: 144542478
    Test: Build aosp targets
    Change-Id: Ie160998150caa953c9a34726525cdbcefc222841
    Merged-In: Ie160998150caa953c9a34726525cdbcefc222841
    (cherry picked from commit 3464451392891c13d458c6a48a7f66b7e234959a)
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>
    Signed-off-by: ImFatF1sh <zijianjiao2017@gmail.com>

--- aosp_dandelion.mk
+++ aosp_dandelion.mk
@@ -17,6 +17,12 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
 # Inherit some common Aosp stuff
 TARGET_BOOT_ANIMATION_RES := 720
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
