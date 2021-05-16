b70bed961da8476d5072692e4c09d5df939ab608
ImFatF1sh <zijianjiao2017@gmail.com>
2021-05-16 11:34:17 +0800

    dandelion: Nuke aosp_base
    Change-Id: I8d8d4734947353688926635e703d63f446ab2cc3
    Signed-off-by: ImFatF1sh <zijianjiao2017@gmail.com>

--- aosp_dandelion.mk
+++ aosp_dandelion.mk
@@ -16,7 +16,6 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Aosp stuff
 TARGET_BOOT_ANIMATION_RES := 720
